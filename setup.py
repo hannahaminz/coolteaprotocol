1ee5618305e34ea1a1a76bd761dccf0134cd89a7
hannahaminz <161190233+hannahaminz@users.noreply.github.com>
2024-03-06 02:52:44 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='high-tea-protocol',
+    version='0.1',
+    description='A high tea protocol tea rank rewards system',
+    url='https://github.com/your-github-username/high-tea-protocol',
+    author='Your Name',
+    author_email='your-email@example.com',
+    license='MIT',
+    packages=['high_tea_protocol'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
